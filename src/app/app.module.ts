500f35ce79a922f6ef435256fb7cbe1fa1865470
kritbovorn <ktaweeyossak@yahoo.com>
2021-02-28 20:17:28 +0700

    Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './header/header.component';
 import { BodyContentComponent } from './body-content/body-content.component';
+import { Lab4Component } from './lab4/lab4.component';
 
 
 
@@ -14,7 +15,8 @@ import { BodyContentComponent } from './body-content/body-content.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyContentComponent
+    BodyContentComponent,
+    Lab4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lab4/lab4.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab4',
+  templateUrl: './lab4.component.html',
+  styleUrls: ['./lab4.component.css']
+})
+export class Lab4Component implements OnInit {
+
+  numberArrays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
