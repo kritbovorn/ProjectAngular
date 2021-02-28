d3ecb3ed1e1ec0ed34b50fe7b74af1d98030c323
kritbovorn <ktaweeyossak@yahoo.com>
2021-02-28 19:13:34 +0700

    Delete CountingComponent and some layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './header/header.component';
-import { CountingComponent } from './counting/counting.component';
 import { BodyContentComponent } from './body-content/body-content.component';
 
 
@@ -15,7 +14,6 @@ import { BodyContentComponent } from './body-content/body-content.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    CountingComponent,
     BodyContentComponent
   ],
   imports: [
