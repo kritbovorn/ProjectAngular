299d0c8d3e4f4f3642a591e51b073bf5e3455a29
kritbovorn <ktaweeyossak@yahoo.com>
2021-03-08 20:06:15 +0700

    Yeh: will try to separate HeaderView

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { Content2Component } from './content2/content2.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    Content2Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
